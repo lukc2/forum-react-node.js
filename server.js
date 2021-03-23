1eedaff4ed5cd78b9e9d7b851efa56955b210e7a
lukc2 <lukc2@wp.pl>
2021-03-23 12:54:36 +0100

    ghj

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-<<<<<<< HEAD
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,24 +19,15 @@ app.use(express.json());
 //please make folders and dedicated files
 
 
-
 if (process.env.NODE_ENV === "production") {
-	
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-	app.use(express.static(path.resolve(__dirname)));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "public", "index.html"));
+	app.get('/', (req, res) => {
+		res.send('Hello World!');
 	});
+	// app.use(express.static(path.resolve(__dirname, "client", "build")));
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	// });
 }
 
-=======
-app.get("*", (req, res) => {
-	app.use(express.static(path.resolve(__dirname, "client", "build")));
-	res.send("hello world");
-});
->>>>>>> parent of b83e895 (fixed small errors +TODO)
 app.listen(PORT, console.log(`http://${HOST}:${PORT}`));
 
