e1a8770cd5a4d7557a224d6aa85a874453a571e4
lukc2 <lukc2@wp.pl>
2021-03-23 01:34:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,13 +20,10 @@ app.use(express.json());
 
 
 if (process.env.NODE_ENV === "production") {
-	app.get('/', (req, res) => {
-		res.send('Hello World!');
+	app.use(express.static(path.resolve(__dirname, "client", "build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
-	// app.use(express.static(path.resolve(__dirname, "client", "build")));
-	// app.get("*", (req, res) => {
-	// 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	// });
 }
 
 app.listen(PORT, console.log(`http://${HOST}:${PORT}`));
