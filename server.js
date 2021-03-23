2543d6fe606bfbe1fa04a490ec0dca396fd04303
lukc2 <lukc2@wp.pl>
2021-03-23 10:16:45 +0100

    a

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+<<<<<<< HEAD
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,5 +33,11 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
+=======
+app.get("*", (req, res) => {
+	app.use(express.static(path.resolve(__dirname, "client", "build")));
+	res.send("hello world");
+});
+>>>>>>> parent of b83e895 (fixed small errors +TODO)
 app.listen(PORT, console.log(`http://${HOST}:${PORT}`));
 
