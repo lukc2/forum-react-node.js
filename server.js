f0afd295fe0cec7d81e6543ba0437096389fc468
lukc2 <lukc2@wp.pl>
2021-03-23 02:08:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 // });
 
 	app.use(express.static(path.resolve(__dirname, "build")));
-	app.get("/", (req, res) => {
+	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "public", "index.html"));
 	});
 }
