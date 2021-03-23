c5a05248fe0c21a51932d699d5e2a50289ab7fbf
lukc2 <lukc2@wp.pl>
2021-03-23 13:39:36 +0100

    a

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
 
-	app.use(express.static(path.resolve(__dirname, "client", "public")));
+	app.use(express.static(path.resolve(__dirname, "client", "build")));
 	app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
 
