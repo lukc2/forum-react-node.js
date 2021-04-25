4e0ef7aa151689764ea2bb0fdfe6f066f6bd160d
ZehirAhsen <62065648+ZehirAhsen@users.noreply.github.com>
2021-04-25 20:43:12 +0200

    Dodano upvoty
    działają, chociaż też zdarzają się errory, ale to fałszywe errory bez treści, bo patrząc po bazie to działa

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -125,16 +125,103 @@ module.exports = {
         db.Thread.update(
             // to jest bardzo dziwne ale działa najlepiej, wymuszamy zmianę updatedAt przez podmianę id na to samo
             // sypie to errora ale działa jak należy, nie wiem co myśleć
-            { id: post.thread_id}, 
-            {
-                where: {
-                    id: post.thread_id
-                }
-            }).then(() => {
+                { id: post.thread_id}, 
+                {where: {id: post.thread_id}}
+        ).then(() => {
+            res.json({
+                success: true,
+                id: thread.id,
+                msg: "Utworzono post"
+            });
+        }).catch(err => {
+            res.json({
+                success: false,
+                errors: err,
+                msg:"Nie udalo sie 2"     
+            }).end();
+        });
+
+    },
+    //http://localhost:3000/api/forum/:categoryId/:threadId PUT
+    votePost : async (req, res) => {
+        let idPost = req.body.postId
+        let userId = req.session.userId
+        const post = await db.Post.findByPk(idPost);
+        let votedString = post.voted
+
+        
+
+        if(votedString == null){
+            votedString = ""
+        }
+        let votArr = votedString.split(",")
+
+        if(votArr.includes(userId.toString())){
+            res.json({
+                success: false,
+                msg: "Ten uzytkownik juz zaglosowal na ten post!"
+            }).end();
+            return; 
+        }
+        votArr.push(userId)
+        let vote = req.body.vote
+        let rep = post.reputation + vote
+        votedString = votArr.join()
+
+        db.Post.update(
+            {reputation: rep, voted: votedString},
+            {where: {id: idPost}}
+        ).then(() => {
                 res.json({
                     success: true,
                     id: thread.id,
-                    msg: "Utworzono post"
+                    msg: "Dodano glos"
+                });
+            }).catch(err => {
+                res.json({
+                    success: false,
+                    errors: err,
+                    msg:"Nie udalo sie 2"
+                    
+                }).end();
+            });
+
+    },
+    voteThread : async (req, res) => {
+        req.session.userId = 3
+        let idThread = req.body.threadId
+        let userId = req.session.userId
+        const thread = await db.Thread.findByPk(idThread);
+        let votedString = thread.voted
+
+        
+
+        if(votedString == null){
+            votedString = ""
+        }
+        let votArr = votedString.split(",")
+
+        if(votArr.includes(userId.toString())){
+            res.json({
+                success: false,
+                msg: "Ten uzytkownik juz zaglosowal na ten wątek!"
+            }).end();
+            return; 
+        }
+        votArr.push(userId)
+        let vote = req.body.vote
+        let rep = thread.reputation + vote
+        votedString = votArr.join()
+
+        db.Thread.update(
+            {reputation: rep, voted: votedString},
+            {where: {id: idThread}}, 
+            {silent : true}
+        ).then(() => {
+                res.json({
+                    success: true,
+                    id: thread.id,
+                    msg: "Dodano glos"
                 });
             }).catch(err => {
                 res.json({
--- routes/api/categoryAPI.js
+++ routes/api/categoryAPI.js
@@ -4,11 +4,17 @@ const { check } = require('express-validator');
 
 router.route("/:categoryId").get(categoryController.viewCategory);
 router.route("/:categoryId/:threadId").get(categoryController.viewThread);
+
 router.route("/:categoryId/addThread").post([
     check("title").isLength({ min: 10 }).withMessage("Zakrótki tytuł wątku"),
     check("content").isLength({ min: 10 }).withMessage("Zakrótka treść wątku")
 ], categoryController.addThread);
-router.route("/:categoryId/:threadId").post(check("content").isLength({ min: 5 }).withMessage("Zakrótka treść posta"), categoryController.addPost);
+router.route("/:categoryId/:threadId").post(
+    check("content").isLength({ min: 5 }).withMessage("Zakrótka treść posta"),
+    categoryController.addPost);
+
+router.route("/:categoryId").put(categoryController.voteThread);
+router.route("/:categoryId/:threadId").put(categoryController.votePost);
 
 
 module.exports = router;
