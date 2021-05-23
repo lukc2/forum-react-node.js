44c00f31fc3931ab6de9c59b658fd00c1d34c03a
ZehirAhsen <62065648+ZehirAhsen@users.noreply.github.com>
2021-05-23 15:22:26 +0200

    adminPanel i pare blędów mniej mam nadzieje

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,11 +4,13 @@ const loginRoutes = require("./loginAPI.js");
 const logoutRoutes = require("./logoutAPI");
 const signInRoutes = require("./signInAPI");
 const userPanelRoutes = require("./userPanelAPI");
+const adminPanelRoutes = require("./adminPanelAPI");
 
 router.use("/forum", categoryRoutes);
 router.use("/login", loginRoutes);
 router.use("/logout", logoutRoutes);
 router.use("/signin", signInRoutes);
 router.use("/userpanel", userPanelRoutes);
+router.use("/adminpanel", adminPanelRoutes);
 
 module.exports = router;
