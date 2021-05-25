0c7359a6331b6737ed8c42b69945507172f83928
ZehirAhsen <62065648+ZehirAhsen@users.noreply.github.com>
2021-05-25 15:00:46 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 
 require("dotenv").config();
 const sequelize = new Sequelize(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_URL)
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
@@ -64,7 +65,7 @@ db.Rank.sync()
     .then(()=>db.User.sync()
     .then(()=>db.Thread.sync()
     .then(()=>db.Post.sync()
-    )))).catch(err => {console.log(err)});
+    ))));
 
 
 ;
