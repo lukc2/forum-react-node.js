33860184ad8057736ec4c98105830dba675b73d1
ZehirAhsen <62065648+ZehirAhsen@users.noreply.github.com>
2021-05-25 16:19:23 +0200

    test cheesa ze stringiem

--- config/database.js
+++ config/database.js
@@ -1,13 +1,7 @@
 const Sequelize = require('sequelize');
 
 require("dotenv").config();
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions:{
-        ssl:true,
-        rejectUnauthorized: false
-    }
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL+'?sslmode=require');
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
