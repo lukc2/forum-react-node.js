a12cb48e5eb86957d441d53ce7bb20cf433332fc
elo240 <61495945+elo240@users.noreply.github.com>
2021-06-11 19:41:02 +0200

    fixed ssl

--- config/database.js
+++ config/database.js
@@ -5,10 +5,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: "postgres",
 	protocol: "postgres",
 	dialectOptions: {
-		// ssl: {
-		// 	require: true,
-		// 	rejectUnauthorized: false,
-		// },
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
 	},
 });
 const db = {};
