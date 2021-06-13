f052ba33e44bec3cda89618e3bf3c360579640cf
lukc2 <lukc2@wp.pl>
2021-06-13 11:53:15 +0200

    last

--- client/src/Site/Category.js
+++ client/src/Site/Category.js
@@ -9,58 +9,9 @@ import axios from "axios";
 export default function Category({ match }) {
 
   let { id } = useParams();
-  const [category, setCategory] = useState([
-    // {
-    //   id: 1,
-    //   name: "Category 1",
-    //   link: "/category/1",
-    // },
-    // {
-    //   id: 2,
-    //   name: "Category 2",
-    //   link: "/category/2",
-    // },
-    // {
-    //   id: 3,
-    //   name: "Category 3",
-    //   link: "/category/3",
-    // }
-  ]);
+  const [category, setCategory] = useState([]);
 
-  const [threads, setThreads] = useState([
-	// 	{
-	// 		id: 1,
-	// 		name: "Hello everyone",
-	// 		category_id: 1,
-	// 		reputation: 14,
-	// 		voted: [],
-	// 		created_at: Date.now(),
-	// 		updated_at: Date.now(),
-	// 		closed: true,
-	// 		user_id: 1,
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		name: "Where do I find this?",
-	// 		category_id: 1,
-	// 		reputation: 23,
-	// 		voted: [],
-	// 		created_at: Date.now(),
-	// 		closed: false,
-	// 		user_id: 1,
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		name: "Why is it so?",
-	// 		category_id: 1,
-	// 		reputation: 3131,
-	// 		voted: [],
-	// 		created_at: Date.now(),
-	// 		closed: false,
-	// 		user_id: 1,
-	// 	},
-	// 
-	]);
+  const [threads, setThreads] = useState([]);
 
 		
 	const getThreads = async () => {
